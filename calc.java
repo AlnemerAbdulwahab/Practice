851d793c4a643ebfe521931673074aac2d74b3ae
Abdulwahab Alnemer <alnemerabdulwahab@gmail.com>
2025-11-28 11:53:54 +0300

    Added add class in calc.java and tested it

--- calc.java
+++ calc.java
@@ -2,9 +2,19 @@ class calculator {
     int ans;
 }
 
+class add extends calculator {
+    int add(int a, int b) {
+        return a + b;
+    }
+}
+
 public class calc {
     public static void main(String[] args) {
         calculator c = new calculator();
+
+        add a = new add();
+        c.ans = a.add(1, 2);
+        System.out.println(c.ans);
         
     }
 }
