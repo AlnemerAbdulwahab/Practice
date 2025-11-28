93b7229900219d8a62010f284ff0fde52ede59a4
Abdulwahab Alnemer <alnemerabdulwahab@gmail.com>
2025-11-28 11:51:08 +0300

    Added mul class in calc.java and tested it

--- calc.java
+++ calc.java
@@ -2,9 +2,19 @@ class calculator {
     int ans;
 }
 
+class mul extends calculator {
+    int mul(int a, int b) {
+        return a * b;
+    }
+}
+
 public class calc {
     public static void main(String[] args) {
         calculator c = new calculator();
+
+        mul m = new mul();
+        c.ans = m.mul(2, 2);
+        System.out.println(c.ans);
         
     }
 }
