0c46cf7d9d5c7597e86230579fa22bd05a64e7ad
Abdulwahab Alnemer <alnemerabdulwahab@gmail.com>
2025-11-28 11:52:48 +0300

    Added div class in calc.java and tested it

--- calc.java
+++ calc.java
@@ -8,12 +8,22 @@ class mul extends calculator {
     }
 }
 
+class div extends calculator {
+    int div(int a, int b) {
+        return a / b;
+    }
+}
+
 public class calc {
     public static void main(String[] args) {
         calculator c = new calculator();
 
         mul m = new mul();
         c.ans = m.mul(2, 2);
+        System.out.println(c.ans);
+
+        div d = new div();
+        c.ans = d.div(4, 2);
         System.out.println(c.ans);
         
     }
