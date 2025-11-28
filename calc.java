c53ed2847e0c2c85937a8c7cf223dbd6b8aeb338
Abdulwahab Alnemer <alnemerabdulwahab@gmail.com>
2025-11-28 11:55:06 +0300

    Added sub class in calc.java and tested it

--- calc.java
+++ calc.java
@@ -8,12 +8,22 @@ class add extends calculator {
     }
 }
 
+class sub extends calculator {
+    int sub(int a, int b) {
+        return a - b;
+    }
+}
+
 public class calc {
     public static void main(String[] args) {
         calculator c = new calculator();
 
         add a = new add();
         c.ans = a.add(1, 2);
+        System.out.println(c.ans);
+
+        sub s = new sub();
+        c.ans = s.sub(2, 1);
         System.out.println(c.ans);
         
     }
